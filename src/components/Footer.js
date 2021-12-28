c39b72dafcfecc641213891546cec08487f2aabc
unknown <rk.rzeszutek@gmail.com>
2021-12-28 11:53:31 +0100

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.css';
+import { BsLinkedin } from 'react-icons/bs';
 
 function Footer() {
     return (
@@ -7,23 +8,23 @@ function Footer() {
             <footer>
                 <div className="footer-content-wrapper">
                     <div className="footer-col large-25 small-50 tiny-100 ta-l flt">
-                        <h3>Portfolio</h3>
-                        <p>Lorem Ipsum is simply dummy text of theprinting and typesetting industry.
+                        <h3 className="footer-header">Portfolio</h3>
+                        <p>Lorem Ipsum is simply dummy text<br></br> of theprinting and typesetting industry.<br></br>
                         Donec venenatis.
                         </p>
                     </div>
                     <div className="footer-col large-25 small-50 tiny-100 ta-l flt">
-                        <h3>Kontakt</h3>
+                        <h3 className="footer-header">Kontakt</h3>
                         <p>rk.rzeszutek@gmail.com</p>
                         <p>692 721 022</p>
+                        <a href="https://www.linkedin.com/in/rafał-rzeszutek-298b0b229/"><BsLinkedin className="icon"></BsLinkedin></a>
                     </div>
                     <div className="footer-col large-25 small-50 tiny-100 ta-l flt">
-                        <h3>Repozytorium</h3>
-                        <a href="https://github.com/rzeszutek/portfolio">https://github.com/rzeszutek/portfolio</a>
-                        <br></br><br></br>
+                        <h3 className="footer-header">Projekt</h3>
+                        <a href="https://github.com/rzeszutek/portfolio">Portfolio React.js</a>
                     </div>
                     <div className="footer-col large-25 small-50 tiny-100 ta-l flt">
-                        <h3>Języki</h3>
+                        <h3 className="footer-header">Języki</h3>
                         <p>English</p>
                         <p>Español</p>
                         <p>日本語版</p>

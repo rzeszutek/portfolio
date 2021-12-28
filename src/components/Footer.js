d47dd7ef08ba4261cb74b29b7b03f1d60e9a9c0e
unknown <rk.rzeszutek@gmail.com>
2021-12-28 11:56:24 +0100

    footer fix v2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,25 +9,25 @@ function Footer() {
                 <div className="footer-content-wrapper">
                     <div className="footer-col large-25 small-50 tiny-100 ta-l flt">
                         <h3 className="footer-header">Portfolio</h3>
-                        <p>Lorem Ipsum is simply dummy text<br></br> of theprinting and typesetting industry.<br></br>
+                        <p className="footer-text">Lorem Ipsum is simply dummy text<br></br> of theprinting and typesetting industry.<br></br>
                         Donec venenatis.
                         </p>
                     </div>
                     <div className="footer-col large-25 small-50 tiny-100 ta-l flt">
                         <h3 className="footer-header">Kontakt</h3>
-                        <p>rk.rzeszutek@gmail.com</p>
-                        <p>692 721 022</p>
-                        <a href="https://www.linkedin.com/in/rafał-rzeszutek-298b0b229/"><BsLinkedin className="icon"></BsLinkedin></a>
+                        <p className="footer-text">rk.rzeszutek@gmail.com</p>
+                        <p className="footer-text">692 721 022</p>
+                        <a className="footer-text" href="https://www.linkedin.com/in/rafał-rzeszutek-298b0b229/"><BsLinkedin className="footer-icon"></BsLinkedin></a>
                     </div>
                     <div className="footer-col large-25 small-50 tiny-100 ta-l flt">
                         <h3 className="footer-header">Projekt</h3>
-                        <a href="https://github.com/rzeszutek/portfolio">Portfolio React.js</a>
+                        <a className="footer-text" href="https://github.com/rzeszutek/portfolio">Portfolio React.js</a>
                     </div>
                     <div className="footer-col large-25 small-50 tiny-100 ta-l flt">
                         <h3 className="footer-header">Języki</h3>
-                        <p>English</p>
-                        <p>Español</p>
-                        <p>日本語版</p>
+                        <p className="footer-text">English</p>
+                        <p className="footer-text">Español</p>
+                        <p className="footer-text">日本語版</p>
                     </div>
                 </div>
                 <div className="clearfix"></div>
